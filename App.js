e7af68cf98cde5edc9ab41c937ff0c9f35f8edfd
Juan Lucero <jlucero@alhpaworks.tech>
2022-05-17 16:18:06 -0700

    feature(google login): added google authorization logic to App.js

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import { StyleSheet, View, Button, Text } from "react-native";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+  const [loggedIn, setLoggedIn] = useState("");
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId:
+      "28332934929-5ss87b86nuakbn8j3h5tmi2sdiqjcvvl.apps.googleusercontent.com",
+    webClientId:
+      "28332934929-ccckm0m9mj90hted4crsr3u1u7mdon99.apps.googleusercontent.com",
+  });
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      const { authentication, type } = response;
+      setLoggedIn(type);
+    }
+  }, [response]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        disabled={!request}
+        title="Login"
+        onPress={() => {
+          promptAsync();
+        }}
+      />
+      <Text>{loggedIn === "success" ? "Logged In" : "Logged Out"}</Text>
     </View>
   );
 }
@@ -13,8 +39,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
